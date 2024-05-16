42ade369cd9743b77d12eeca95d023d5c18a3a50
bohdan <shovkopliasbohdan@gmail.com>
2024-05-17 01:38:44 +0300

    UserContext

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,30 @@
+import { ReactNode, createContext, useContext } from "react";
+import { UserContextProps, currentUser } from "../queries/userQueries";
+import { useQuery } from "@tanstack/react-query";
+
+const UserContext = createContext<UserContextProps | null | undefined>(null);
+
+function UserContextProvider({ children }: { children: ReactNode }) {
+   const { isPending, isError, data, error } = useQuery({
+      queryKey: [`currentUser`],
+      queryFn: currentUser
+   });
+
+   let contextData: UserContextProps | null | undefined = null;
+
+   if (isPending) contextData = undefined;
+   else if (isError) contextData = null;
+   else if (data) contextData = data;
+
+   return (
+      <UserContext.Provider value={contextData}>
+         {children}
+      </UserContext.Provider>
+   );
+}
+
+const useUser = () => {
+   return useContext(UserContext);
+};
+
+export { UserContextProvider, useUser };
