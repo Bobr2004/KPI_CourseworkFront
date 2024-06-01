0965508ad5c4179b9fb4efb3a92ed36f34c77504
bohdan <shovkopliasbohdan@gmail.com>
2024-06-01 22:41:35 +0300

    submitSendTestData

--- src/mutations/testMutations.tsx
+++ src/mutations/testMutations.tsx
@@ -11,9 +11,25 @@ type submitTestResponseData = {
    receivedPoints: number;
    points: number;
 };
+
+type sendTestDataType = {
+   testId: number;
+   quizzes: quizType[];
+};
+
+type quizType = [id: number, answer: string];
+
 const submitTest = async (submitTest: submitTestData) => {
+   const quizzes = Object.entries(submitTest).filter(
+      (el) => el[0] !== "testId"
+   );
+   const sendData = {
+      testId: submitTest.testId,
+      quizzes
+   };
+
    const { data }: { data: submitTestResponseData } =
-      await learnCredintialsAPI.post(`/submit-test`, submitTest);
+      await learnCredintialsAPI.post(`/submit-test`, sendData);
    return data;
 };
 
